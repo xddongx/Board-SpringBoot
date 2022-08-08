28218708d4eef35a6c2768b5ca798e16d666a151
xddongx <rlaehdgus21@hanmail.net>
2022-08-08 17:48:37 +0900

    api GET[board read one, board read list], PUT[board modified]

--- src/main/java/site/xddongx/boardspringboot/controller/BoardAPIController.java
+++ src/main/java/site/xddongx/boardspringboot/controller/BoardAPIController.java
@@ -1,5 +1,6 @@
 package site.xddongx.boardspringboot.controller;
 
+import org.apache.coyote.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -7,8 +8,10 @@ import org.springframework.web.bind.annotation.*;
 import site.xddongx.boardspringboot.dto.BoardDto;
 import site.xddongx.boardspringboot.servie.BoardService;
 
+import java.util.Collection;
+
 @RestController
-@RequestMapping("api")
+@RequestMapping("api/board")
 public class BoardAPIController {
     private final Logger logger = LoggerFactory.getLogger(BoardAPIController.class);
     private final BoardService boardService;
@@ -18,15 +21,25 @@ public class BoardAPIController {
     }
 
     // GET
-
+    @GetMapping()
+    public ResponseEntity<Collection<BoardDto>> readBoarList() {
+        return ResponseEntity.ok(this.boardService.readBoardList());
+    }
     // GET
-
+    @GetMapping("{postNo}")
+    public ResponseEntity<BoardDto> readBoardOne(@PathVariable("postNo") Long postNo) {
+        return ResponseEntity.ok(this.boardService.readBoardOne(postNo));
+    }
     // POST
-    @PostMapping("board")
-    public ResponseEntity<BoardDto> createBaord(@RequestBody BoardDto boardDto) {
+    @PostMapping()
+    public ResponseEntity<BoardDto> createBoard(@RequestBody BoardDto boardDto) {
         return ResponseEntity.ok(this.boardService.createBoard(boardDto));
     }
     // PUT
+    @PutMapping("{postNo}")
+    public ResponseEntity<BoardDto> modifiedBoard(@PathVariable("postNo") Long postNo, @RequestBody BoardDto boardDto) {
+        return ResponseEntity.ok(this.boardService.modifiedBoard(postNo, boardDto));
+    }
 
     // DELETE
 
--- src/main/java/site/xddongx/boardspringboot/servie/BoardService.java
+++ src/main/java/site/xddongx/boardspringboot/servie/BoardService.java
@@ -2,10 +2,16 @@ package site.xddongx.boardspringboot.servie;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import site.xddongx.boardspringboot.dto.BoardDto;
 import site.xddongx.boardspringboot.entity.Board;
 import site.xddongx.boardspringboot.repository.BoardRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BoardService {
@@ -28,5 +34,32 @@ public class BoardService {
         this.boardRepository.save(newBoard);
 
         return new BoardDto(newBoard);
+    }
+
+    public List<BoardDto> readBoardList() {
+        List<BoardDto> boardDtoList = new ArrayList<>();
+        boardRepository.findAll().forEach(board -> boardDtoList.add(new BoardDto(board)));
+
+        return boardDtoList;
+    }
+
+    public BoardDto readBoardOne(Long postNo) {
+        Optional<Board> board = boardRepository.findById(postNo);
+        if (board.isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        return new BoardDto(board.get());
+    }
+
+    public BoardDto modifiedBoard(Long postNo, BoardDto boardDto) {
+        Optional<Board> boardOptional = boardRepository.findById(postNo);
+
+        if(boardOptional.isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        Board board = boardOptional.get();
+        board.setTitle(boardDto.getTitle());
+        board.setContent(board.getContent());
+
+        boardRepository.save(board);
+        return new BoardDto(board);
     }
 }
