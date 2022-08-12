3da6860d9d95bc7f0205d67399bb6d9206bd989b
xddongx <rlaehdgus21@hanmail.net>
2022-08-12 14:39:30 +0900

    view board list

---
+++ src/main/java/site/xddongx/boardspringboot/controller/HomeController.java
@@ -0,0 +1,13 @@
+package site.xddongx.boardspringboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping()
+    public String home(){
+        return "home";
+    }
+}
